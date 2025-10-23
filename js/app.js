933e9f6f9de1caba5f6099693476285b8e37e84c
zairjmg <jair.jmgu@gmail.com>
2025-10-23 00:45:27 -0600

    menu pantalla completa

--- js/app.js
+++ js/app.js
@@ -17,4 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
             menuBtn.innerHTML = '<i class="fas fa-bars"></i>';
         }
     });
+
+    // Sticky header
+    window.addEventListener("scroll", () => {
+        const header = document.querySelector("header");
+        if(window.scrollY > 50){
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+    });
 });
