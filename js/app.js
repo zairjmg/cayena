d7fbdcb54746b56e69174a5b6cc79accafb6a464
zairjmg <jair.jmgu@gmail.com>
2025-10-25 16:02:24 -0600

    agregue articulo

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let nav = document.querySelector("header nav");
-    let menuBtn = document.createElement("button");
+  let nav = document.querySelector("header nav");
+  let menuBtn = document.createElement("button");
 
-    menuBtn.innerHTML = '<i class="fas fa-bars"></i>';
-    menuBtn.classList.add("menu-toggle");
+  menuBtn.innerHTML = '<i class="fas fa-bars"></i>';
+  menuBtn.classList.add("menu-toggle");
 
-    document.querySelector("header").insertBefore(menuBtn, nav);
+  document.querySelector("header").insertBefore(menuBtn, nav);
 
-    menuBtn.addEventListener("click", () => {
-        nav.classList.toggle("active");
-        menuBtn.classList.toggle("open");
+  menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    menuBtn.classList.toggle("open");
 
-        if (menuBtn.classList.contains("open")) {
-            menuBtn.innerHTML = '<i class="fas fa-times"></i>';
-        } else {
-            menuBtn.innerHTML = '<i class="fas fa-bars"></i>';
-        }
-    });
+    if (menuBtn.classList.contains("open")) {
+      menuBtn.innerHTML = '<i class="fas fa-times"></i>';
+      const i_aos = document.querySelector("i")
 
-    // Sticky header
-    window.addEventListener("scroll", () => {
-        const header = document.querySelector("header");
-        if(window.scrollY > 50){
-            header.classList.add("scrolled");
-        } else {
-            header.classList.remove("scrolled");
-        }
-    });
+      i_aos.style.color = "#000"
+      i_aos.style.transition = "color .3s ease"
+
+    } else {
+      menuBtn.innerHTML = '<i class="fas fa-bars"></i>';
+    }
+  });
+
+  // Sticky header
+  window.addEventListener("scroll", () => {
+      const header = document.querySelector("header");
+      if(window.scrollY > 50){
+          header.classList.add("scrolled");
+      } else {
+          header.classList.remove("scrolled");
+      }
+  });
 });
 
 // Animaciones tipo AOS personalizadas
